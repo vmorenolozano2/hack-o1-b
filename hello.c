bfffa72f11d94386786b8d5ff29689868d66336e
Victor Moreno Lozano <vmorenoloz>
2018-08-23 20:40:18 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Victor Moreno
+ * Date: 08/21/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Victor Moreno computer engineering!\n");
+
+  return 0;
+}
